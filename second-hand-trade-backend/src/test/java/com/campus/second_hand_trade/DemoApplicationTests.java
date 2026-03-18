a3a7b73b28d082498b801648c4f73b3e687bb14a
王凯涛 <wangkt@archempower.com>
2026-03-18 20:12:49 +0800

    12

--- second-hand-trade-backend/src/test/java/com/campus/second_hand_trade/DemoApplicationTests.java
+++ second-hand-trade-backend/src/test/java/com/campus/second_hand_trade/DemoApplicationTests.java
@@ -2,8 +2,9 @@ package com.campus.second_hand_trade;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.campus.secondhand.DemoApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = DemoApplication.class)
 class DemoApplicationTests {
 
 	@Test
